42383827e9f7101a4c9a21c93545291cc27a771c
AhmedSaidCetiner <f211220030@ktun.edu.tr>
2025-05-02 02:38:54 +0300

    projenin development ve production alanları ayrıştırıldı.

--- Presentation/YoutubeApi.Api/Program.cs
+++ Presentation/YoutubeApi.Api/Program.cs
@@ -4,14 +4,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var env = builder.Environment;
+
+builder.Configuration
+    .SetBasePath(env.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
