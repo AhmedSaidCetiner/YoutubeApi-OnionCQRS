fa0cc2a733715684e01693054622191939f5cdd8
AhmedSaidCetiner <f211220030@ktun.edu.tr>
2025-05-16 11:36:24 +0300

    identity db'ye geçildi.

--- Infrastructure/YoutubeApi.Persistence/Registration.cs
+++ Infrastructure/YoutubeApi.Persistence/Registration.cs
@@ -6,6 +6,7 @@ using YoutubeApi.Application.Interfaces.Repositories;
 using YoutubeApi.Persistence.Repositories;
 using YoutubeApi.Application.Interfaces.UnitOfWorks;
 using YoutubeApi.Persistence.UnitOfWorks;
+using YoutubeApi.Domain.Entities;
 
 namespace YoutubeApi.Persistence
 {
@@ -20,6 +21,18 @@ namespace YoutubeApi.Persistence
             services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddIdentityCore<User>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 2;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireDigit = false;
+                opt.SignIn.RequireConfirmedEmail = false;
+            })
+                .AddRoles<Role>()
+                .AddEntityFrameworkStores<AppDbContext>();
         }
     }
 }
