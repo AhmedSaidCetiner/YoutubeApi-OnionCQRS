2ed45deda274eec319b29e8f81cc2317edfad779
AhmedSaidCetiner <f211220030@ktun.edu.tr>
2025-05-17 12:15:37 +0300

    Jwt ile login işlemleri yapıldı

---
+++ Core/YoutubeApi.Application/Features/Auth/Command/Login/LoginCommandRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System.ComponentModel;
+
+namespace YoutubeApi.Application.Features.Auth.Command.Login
+{
+    public class LoginCommandRequest : IRequest<LoginCommandResponse>
+    {
+        [DefaultValue("acetiner@gmail.com")]
+        public string Email { get; set; }
+        [DefaultValue("03421192")]
+        public string Password { get; set; }
+    }
+}
